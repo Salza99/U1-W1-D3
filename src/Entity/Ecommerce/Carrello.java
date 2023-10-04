7b9c3ac88f03b74d005a4a08821519ceb0053d41
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-10-04 16:49:26 +0200

    risolved Switch case problem, find out i need a loop to proceed

--- src/Entity/Ecommerce/Carrello.java
+++ src/Entity/Ecommerce/Carrello.java
@@ -8,4 +8,20 @@ public class Carrello {
     public Carrello(Cliente clienteAssociato) {
         this.clienteAssociato = clienteAssociato;
     }
+
+    public Articolo[] getListaArticoli() {
+        return listaArticoli;
+    }
+
+    public void setListaArticoli(int n1, Articolo listaArticoli) {
+        this.listaArticoli[n1] = listaArticoli;
+    }
+
+    public double getCostoTotale() {
+        return costoTotale;
+    }
+
+    public void setCostoTotale(double costoTotale) {
+        this.costoTotale = costoTotale;
+    }
 }
--- src/ExerciseThreeMain.java
+++ src/ExerciseThreeMain.java
@@ -3,6 +3,8 @@ import Entity.Ecommerce.Carrello;
 import Entity.Ecommerce.Cliente;
 
 import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ExerciseThreeMain {
     public static void main(String[] args) {
@@ -10,8 +12,8 @@ public class ExerciseThreeMain {
         //creo gli articoli del mio negozio
         Articolo tShirt = new Articolo("color: black, size: 'M', 100% cotton", 9.99);
         Articolo hat = new Articolo("color: white, 100% cotton", 19.99);
-        Articolo shoe = new Articolo("color: brown, elegant", 39.99);
-        Articolo Sweatshirt = new Articolo("color: white, size: 'M', 100% cotton", 14.99);
+        Articolo shoes = new Articolo("color: brown, elegant", 39.99);
+        Articolo sweatshirt = new Articolo("color: white, size: 'M', 100% cotton", 14.99);
         Articolo trousers = new Articolo("color: darkBlue, size: '44', 100% cotton", 24.99);
 
         // creo un evento Data da inserire nel costruttore del cliente
@@ -26,5 +28,56 @@ public class ExerciseThreeMain {
 
         Carrello aC = new Carrello(a);
         Carrello bC = new Carrello(b);
+
+        //creo un applicativo in console
+        runApplication(a, b, tShirt, hat, shoes, sweatshirt, trousers );
+
+        //controllo per chiusura applicazione
+        String close = ":q";
+
     }
+    public static void runApplication (Cliente a, Cliente b, Articolo tShirt, Articolo hat, Articolo shoes, Articolo sweatshirt, Articolo trousers) {
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("seleziona un utente (a, b)");
+            String resp = input.nextLine();
+            Cliente selezionato = resp.equals("a") ? a : b;
+            Carrello carrelloAssociato = new Carrello(selezionato);
+            System.out.println("Benvenuto " + selezionato.getAnagraphic());
+            System.out.println("selezioni un articolo da mettere nel carrello (tShirt, hat, shoes, sweatshirt, trousers");
+            String articleSelected = input.nextLine();
+            switch (articleSelected) {
+                case "tshirt":
+                    carrelloAssociato.setListaArticoli(0, tShirt);
+                    System.out.println(carrelloAssociato.getListaArticoli());
+                    break;
+                case "hat":
+                    carrelloAssociato.setListaArticoli(1, hat);
+                    System.out.println(carrelloAssociato.getListaArticoli());
+                    break;
+                case "shoes":
+                    carrelloAssociato.setListaArticoli(2, shoes);
+                    System.out.println(carrelloAssociato.getListaArticoli());
+                    break;
+                case "sweatshirt":
+                    carrelloAssociato.setListaArticoli(3, sweatshirt);
+                    System.out.println(carrelloAssociato.getListaArticoli());
+                    break;
+                case "trousers":
+                    carrelloAssociato.setListaArticoli(4, trousers);
+                    System.out.println(carrelloAssociato.getListaArticoli());
+                    break;
+                default:
+                    System.out.println("Articolo non valido.");
+                    break;
+            }
+            System.out.println("Si desiderano fare altri acquisti? (s, n)");
+            String respSecond = input.nextLine();
+            if (respSecond.equals("s")) {
+                System.out.println("selezioni un articolo da mettere nel carrello (tShirt, hat, shoes, sweatshirt, trousers");
+
+            }
+        }
+    }
+
 }
